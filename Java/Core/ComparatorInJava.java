ff74dfdb636e1f8a75878e15a6434e8a7ad1a4e0
Rupesh <rupeshaadhikari1@gmail.com>
2024-06-19 12:05:31 +0530

    Practised Comperator in Java

--- Java/Core/ComparatorInJava.java
+++ Java/Core/ComparatorInJava.java
@@ -1,4 +1,5 @@
-package CORE;
+package Java.Core;
+
 /* Comparator Interface in JAVA 
  * Collection  : Interface
  * Collections : Class
@@ -9,25 +10,47 @@ import java.util.Collections;
 import java.util.Comparator;
 
 class Student {
-    
+
     int age;
     String name;
-    
+
     public Student(int age, String name) {
         this.age = age;
         this.name = name;
     }
 
     public String toString() {
-        return "Student [age=" + age + ", name=" + name + "]";
+        return name + age;
+        // return name;
+        // return "name : " + name;
+        // return "age : " + age;
+        // return "Student [age=" + age + ", name=" + name + "]";
     }
 
+}
+
+// compare by name
+class ComparebyName implements Comparator<Student> {
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.name.compareTo(s2.name);
+    }
+}
+
+// compare by age
+class CompareByAge implements Comparator<Student> {
+
+    @Override
+    public int compare(Student s1, Student s2) {
+        return Integer.compare(s1.age, s2.age);
+    }
 }
 
 public class ComparatorInJava {
 
     public static void main(String[] args) {
-        
+
         List<Integer> age = new ArrayList<>();
         age.add(20);
         age.add(29);
@@ -38,28 +61,25 @@ public class ComparatorInJava {
         age.add(66);
         age.add(27);
 
-        System.out.println("Before Sorting : "+ age);
-        Collections.sort(age);
-        System.out.println("After Sorting : " + age);
-        
+        // System.out.println("Before Sorting : " + age);
+        // Collections.sort(age);
+        // System.out.println("After Sorting : " + age);
 
         // Comperator Intrface
-        Comparator<Integer> com = new Comparator<Integer>(){
+        Comparator<Integer> com = new Comparator<Integer>() {
 
-            public int compare(Integer i, Integer j){
+            public int compare(Integer i, Integer j) {
 
-                if(i%10 > j%10){
+                if (i % 10 > j % 10) {
                     return 1;
-                }
-                else{
+                } else {
                     return -1;
                 }
             }
         };
 
-        Collections.sort(age, com);
-        System.out.println(age);
-
+        // Collections.sort(age, com);
+        // System.out.println(age);
 
         // List of Students Objects
         List<Student> studs = new ArrayList<>();
@@ -68,18 +88,22 @@ public class ComparatorInJava {
         studs.add(new Student(35, "Ritesh"));
         studs.add(new Student(95, "Milan"));
         studs.add(new Student(48, "Aman"));
+        System.out.println("before sorting age using comperator :" + studs);
+        Collections.sort(studs, new Comparator<Student>() {
+            @Override
+            public int compare(Student s1, Student s2) {
+                return Integer.compare(s1.age, s2.age);
+            }
+        });
 
+        // Collections.sort(studs, null);
+        System.out.println("after  sorting age using comperator :" + studs);
 
-        Collections.sort(studs, null);
-        // Comparator<String> comStr = new Comparator<String>();
-        // {
-        //     public void compare(String s1, String s2){
-                
-        //     }
-        // }
-
-   
-
+        // System.out.println("before sorting by name :");
+        Collections.sort(studs, new ComparebyName());
+        System.out.println("after sorting by name :" + studs);
+        Collections.sort(studs, new CompareByAge());
+        System.out.println("after sorting by age :" + studs);
     }
-    
+
 }
