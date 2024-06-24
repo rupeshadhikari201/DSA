04c7e224e45db5ab9b777e99f523c4b47edd7cf3
Rupesh <rupeshaadhikari1@gmail.com>
2024-06-24 21:24:41 +0530

    Flood Fill Algorithm using Bfs

---
+++ Java/Graph/FloodFillAlgorithmBfs.java
@@ -0,0 +1,17 @@
+package Graph;
+
+public class FloodFillAlgorithmBfs {
+    public static void main(String[] args) {
+
+        int[][] image = { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } };
+        int sr = 1;
+        int sc = 1;
+        int newColor = 2;
+        // Output: {{2,2,2},{2,2,0},{2,0,1}}
+    }
+
+    public static int[][] floodFillBfs(int[][] image, int sr, int sc, int newColor) {
+
+        return image;
+    }
+}
